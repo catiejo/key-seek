65e71bb73306174501f68e3dce0d817f302eb01b
catiejojo <catiejo.pidel@gmail.com>
2016-10-05 00:42:03 +0200

    Initial Commit: Sound Hide & Seek

---
+++ Assets/GameBehavior.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBehavior : MonoBehaviour {
+	public AudioClip wrongSound;
+	public AudioClip rightSound;
+
+	// Use this for initialization
+	void Start () 
+	{
+		HideKey ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void HideKey () {
+		SmashObject[] hidingSpots = FindObjectsOfType(typeof(SmashObject)) as SmashObject[];
+		foreach (SmashObject spot in hidingSpots) {
+			AudioSource audioSource = spot.GetComponent<AudioSource>();
+			audioSource.clip = wrongSound;
+			audioSource.pitch = Random.Range (0.25f, 1.75f);;
+		}
+		int hidingSpot = Random.Range (0, hidingSpots.Length);
+		Debug.Log("spot is " + hidingSpot);
+		AudioSource winner = hidingSpots [hidingSpot].GetComponent<AudioSource> ();
+		winner.clip = rightSound;
+		winner.pitch = 1.0f;
+	}
+
+	void StartTimer () {
+		
+	}
+		
+}
---
+++ Assets/SmashObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmashObject : MonoBehaviour {
+	public AudioSource smashSound;
+	bool isMagic;
+	private GameBehavior _controller;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Find the game controller object in the scene
+		GameObject controllerObject = GameObject.FindWithTag ("GameController");
+		if (controllerObject != null) {
+			_controller = controllerObject.GetComponent<GameBehavior> ();
+			smashSound = gameObject.GetComponent<AudioSource>();
+		} else {
+			Debug.Log ("SmashObject cannot find game controller.");
+		}
+	}
+
+	void OnMouseDown () {
+		smashSound.Play ();
+		Debug.Log ("ouch!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
